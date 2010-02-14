7a35dbe2276843b4395550589cfec6daf33f72a8
steffen.schafer <steffen.schafer@6236e0a2-f3dd-11de-acd2-8fd6687fc178>
2010-02-14 17:34:07 +0000

    - added exception handling - changed project name

--- GwtGL-examples/src/main/java/com/googlecode/gwtgl/example/client/Gwt3d_example.java
+++ GwtGL-examples/src/main/java/com/googlecode/gwtgl/example/client/Gwt3d_example.java
@@ -1,6 +1,7 @@
 package com.googlecode.gwtgl.example.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TabPanel;
 import com.googlecode.gwtgl.example.client.examples.coloredtriangle.wrapper.ColoredTriangleWrapperExample;
@@ -20,24 +21,28 @@ public class Gwt3d_example implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		TabPanel tp = new TabPanel();
-
-		WhiteTriangleExample whiteTriangleExample = new WhiteTriangleExample();
-		tp.add(whiteTriangleExample, "White Triangle (Binding)");
-
-		WhiteTriangleWrapperExample whiteTriangleWrapperExample = new WhiteTriangleWrapperExample();
-		tp.add(whiteTriangleWrapperExample, "White Triangle (Wrapper)");
-
-		ColoredTriangleWrapperExample coloredTriangleWrapperExample = new ColoredTriangleWrapperExample();
-		tp.add(coloredTriangleWrapperExample, "Colored Triangle (Wrapper)");
-
-		TexturedCubeWrapperExample texturedCubeWrapperExample = new TexturedCubeWrapperExample();
-		tp.add(texturedCubeWrapperExample, "Textured Cube (Wrapper)");
-
-		LightingWrapperExample lightingWrapperExample = new LightingWrapperExample();
-		tp.add(lightingWrapperExample, "Lighting (Wrapper)");
-
-		tp.selectTab(3);
-		RootPanel.get("gwt3d-example").add(tp);
+		try{
+			TabPanel tp = new TabPanel();
+	
+			WhiteTriangleExample whiteTriangleExample = new WhiteTriangleExample();
+			tp.add(whiteTriangleExample, "White Triangle (Binding)");
+	
+			WhiteTriangleWrapperExample whiteTriangleWrapperExample = new WhiteTriangleWrapperExample();
+			tp.add(whiteTriangleWrapperExample, "White Triangle (Wrapper)");
+	
+			ColoredTriangleWrapperExample coloredTriangleWrapperExample = new ColoredTriangleWrapperExample();
+			tp.add(coloredTriangleWrapperExample, "Colored Triangle (Wrapper)");
+	
+			TexturedCubeWrapperExample texturedCubeWrapperExample = new TexturedCubeWrapperExample();
+			tp.add(texturedCubeWrapperExample, "Textured Cube (Wrapper)");
+	
+			LightingWrapperExample lightingWrapperExample = new LightingWrapperExample();
+			tp.add(lightingWrapperExample, "Lighting (Wrapper)");
+	
+			tp.selectTab(3);
+			RootPanel.get("gwt3d-example").add(tp);
+		} catch (Exception e) {
+			Window.alert(e.getMessage());
+		}
 	}
 }
