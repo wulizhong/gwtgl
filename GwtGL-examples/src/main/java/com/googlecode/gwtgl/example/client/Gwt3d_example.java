7a5b47e768e2d08f6e1ccc81c6ee217588b5601a
steffen.schafer <steffen.schafer@6236e0a2-f3dd-11de-acd2-8fd6687fc178>
2010-02-14 16:35:50 +0000

    initial import of the examples project

---
+++ GwtGL-examples/src/main/java/com/googlecode/gwtgl/example/client/Gwt3d_example.java
@@ -0,0 +1,43 @@
+package com.googlecode.gwtgl.example.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TabPanel;
+import com.googlecode.gwtgl.example.client.examples.coloredtriangle.wrapper.ColoredTriangleWrapperExample;
+import com.googlecode.gwtgl.example.client.examples.lighting.wrapper.LightingWrapperExample;
+import com.googlecode.gwtgl.example.client.examples.texturedcube.wrapper.TexturedCubeWrapperExample;
+import com.googlecode.gwtgl.example.client.examples.whitetriangle.WhiteTriangleExample;
+import com.googlecode.gwtgl.example.client.examples.whitetriangle.wrapper.WhiteTriangleWrapperExample;
+
+/**
+ * @author Steffen Schäfer
+ * @author Sönke Sothmann
+ * 
+ */
+public class Gwt3d_example implements EntryPoint {
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		TabPanel tp = new TabPanel();
+
+		WhiteTriangleExample whiteTriangleExample = new WhiteTriangleExample();
+		tp.add(whiteTriangleExample, "White Triangle (Binding)");
+
+		WhiteTriangleWrapperExample whiteTriangleWrapperExample = new WhiteTriangleWrapperExample();
+		tp.add(whiteTriangleWrapperExample, "White Triangle (Wrapper)");
+
+		ColoredTriangleWrapperExample coloredTriangleWrapperExample = new ColoredTriangleWrapperExample();
+		tp.add(coloredTriangleWrapperExample, "Colored Triangle (Wrapper)");
+
+		TexturedCubeWrapperExample texturedCubeWrapperExample = new TexturedCubeWrapperExample();
+		tp.add(texturedCubeWrapperExample, "Textured Cube (Wrapper)");
+
+		LightingWrapperExample lightingWrapperExample = new LightingWrapperExample();
+		tp.add(lightingWrapperExample, "Lighting (Wrapper)");
+
+		tp.selectTab(3);
+		RootPanel.get("gwt3d-example").add(tp);
+	}
+}
