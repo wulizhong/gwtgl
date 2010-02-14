03cf6cdc08543ac4d728835f54d07e8ec6f43fc2
soenke.sothmann <soenke.sothmann@6236e0a2-f3dd-11de-acd2-8fd6687fc178>
2010-02-14 16:05:19 +0000

    modified licence headers

--- GwtGL/src/main/java/com/googlecode/gwtgl/binding/WebGLActiveInfo.java
+++ GwtGL/src/main/java/com/googlecode/gwtgl/binding/WebGLActiveInfo.java
@@ -1,5 +1,5 @@
 /**   
- * Copyright 2009-2010 Sönke Sothmann & Steffen Schäfer
+ * Copyright 2009-2010 Sönke Sothmann, Steffen Schäfer and others
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
