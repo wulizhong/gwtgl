2016bd862713f4fd136bdcb3d2c58fbcd3d9ac56
steffen.schafer <steffen.schafer@gmail.com>
2011-01-01 14:41:22 +0000

    moved all wrapper examples to the techdemos

--- GwtGL-examples/src/main/java/com/googlecode/gwtgl/example/client/GwtGL_examples.java
+++ GwtGL-examples/src/main/java/com/googlecode/gwtgl/example/client/GwtGL_examples.java
@@ -5,14 +5,8 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TabPanel;
 import com.googlecode.gwtgl.example.client.examples.coloredtriangle.binding.ColoredTriangleBindingExample;
-import com.googlecode.gwtgl.example.client.examples.coloredtriangle.wrapper.ColoredTriangleWrapperExample;
-import com.googlecode.gwtgl.example.client.examples.lighting.wrapper.LightingWrapperExample;
-import com.googlecode.gwtgl.example.client.examples.skybox.wrapper.SkyboxWrapperExample;
 import com.googlecode.gwtgl.example.client.examples.texturedcube.binding.TexturedCubeBindingExample;
-import com.googlecode.gwtgl.example.client.examples.texturedcube.wrapper.TexturedCubeWrapperExample;
-import com.googlecode.gwtgl.example.client.examples.texturedsphere.wrapper.TexturedSphereWrapperExample;
 import com.googlecode.gwtgl.example.client.examples.whitetriangle.WhiteTriangleExample;
-import com.googlecode.gwtgl.example.client.examples.whitetriangle.wrapper.WhiteTriangleWrapperExample;
 
 /**
  * @author Steffen Schäfer
@@ -29,33 +23,15 @@ public class GwtGL_examples implements EntryPoint {
 			TabPanel tp = new TabPanel();
 	
 			WhiteTriangleExample whiteTriangleExample = new WhiteTriangleExample();
-			tp.add(whiteTriangleExample, "White Triangle (Binding)");
+			tp.add(whiteTriangleExample, "White Triangle");
 			
 			ColoredTriangleBindingExample coloredTriangleBindingExample = new ColoredTriangleBindingExample();
-			tp.add(coloredTriangleBindingExample, "Colored Triangle (Binding)");
+			tp.add(coloredTriangleBindingExample, "Colored Triangle");
 			
 			TexturedCubeBindingExample texturedCubeBindingExample = new TexturedCubeBindingExample();
-			tp.add(texturedCubeBindingExample, "Textured Cube (Binding)");
+			tp.add(texturedCubeBindingExample, "Textured Cube");
 	
-			WhiteTriangleWrapperExample whiteTriangleWrapperExample = new WhiteTriangleWrapperExample();
-			tp.add(whiteTriangleWrapperExample, "White Triangle (Wrapper)");
-	
-			ColoredTriangleWrapperExample coloredTriangleWrapperExample = new ColoredTriangleWrapperExample();
-			tp.add(coloredTriangleWrapperExample, "Colored Triangle (Wrapper)");
-	
-			TexturedCubeWrapperExample texturedCubeWrapperExample = new TexturedCubeWrapperExample();
-			tp.add(texturedCubeWrapperExample, "Textured Cube (Wrapper)");
-			
-			TexturedSphereWrapperExample texturedSphereWrapperExample = new TexturedSphereWrapperExample();
-			tp.add(texturedSphereWrapperExample, "Textured Sphere (Wrapper)");
-	
-			LightingWrapperExample lightingWrapperExample = new LightingWrapperExample();
-			tp.add(lightingWrapperExample, "Lighting (Wrapper)");
-			
-			SkyboxWrapperExample skyboxWrapperExample = new SkyboxWrapperExample();
-			tp.add(skyboxWrapperExample, "Skybox (Wrapper)");
-	
-			tp.selectTab(3);
+			tp.selectTab(2);
 			RootPanel.get("gwtgl-examples").add(tp);
 		} catch (Exception e) {
 			Window.alert("Sorry, Your Browser doesn't support WebGL!");
