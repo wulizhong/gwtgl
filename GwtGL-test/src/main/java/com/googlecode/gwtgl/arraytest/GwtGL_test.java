7187427b93e5c2ffa27d166a0e162b19061557d4
steffen.schafer <steffen.schafer@gmail.com>
2011-07-10 18:23:05 +0000

    initial import of tests for the typed arrays

---
+++ GwtGL-test/src/main/java/com/googlecode/gwtgl/arraytest/GwtGL_test.java
@@ -0,0 +1,15 @@
+package com.googlecode.gwtgl.arraytest;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class GwtGL_test implements EntryPoint {
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+	}
+}
