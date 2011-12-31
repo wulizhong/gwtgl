2bccbd11aa01d0600bb5d595754932de4d016e74
soenke.sothmann@gmail.com <soenke.sothmann@gmail.com@6236e0a2-f3dd-11de-acd2-8fd6687fc178>
2011-12-31 13:35:43 +0000

    Added Tests for Uint32Array

---
+++ trunk/GwtGL/src/test/java/com/google/gwt/typedarrays/client/Uint32ArrayTest.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2009-2011 Sönke Sothmann, Steffen Schäfer and others
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.gwt.typedarrays.client;
+
+import com.google.gwt.core.client.JsArrayInteger;
+import com.google.gwt.junit.DoNotRunWith;
+import com.google.gwt.junit.Platform;
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * Tests for {@link Uint32Array}.
+ */
+@DoNotRunWith(Platform.HtmlUnitUnknown)
+public class Uint32ArrayTest extends GWTTestCase {
+  private static final int numBytes = 4;
+
+  private static final int[] testData = new int[] {0, 1, 2, 3, Integer.MAX_VALUE};
+
+  private static final int[] testDataPart1 = new int[] {0, 1, 2, 3};
+
+  private static final int[] testDataPart2 = new int[] {Integer.MAX_VALUE};
+
+  private static native JsArrayInteger testDataJsArray() /*-{
+    return [ 0, 1, 2, 3, 2147483647 ];
+  }-*/;
+
+  private static native JsArrayInteger testDataJsArrayPart1() /*-{
+    return [ 0, 1, 2, 3 ];
+  }-*/;
+
+  private static native JsArrayInteger testDataJsArrayPart2() /*-{
+    return [ 2147483647 ];
+  }-*/;
+
+  // 2^32-1, 0, 0-1, 2^32
+  private static native JsArrayInteger testDataValueRange() /*-{
+    return [ 4294967295, 0, -1, 4294967296 ];
+  }-*/;
+
+  @Override
+  public String getModuleName() {
+    return "com.google.gwt.typedarrays.TypedArrays";
+  }
+
+  public void testInitWithArrayBuffer() {
+    ArrayBuffer arrayBuffer = ArrayBuffer.create(testData.length * numBytes);
+
+    Uint32Array array = Uint32Array.create(arrayBuffer);
+
+    array.set(testData);
+    assertIsTestData(array);
+  }
+
+  public void testInitWithArrayBufferAndOffset() {
+    ArrayBuffer arrayBuffer = ArrayBuffer.create((testData.length + 1) * numBytes);
+
+    Uint32Array array = Uint32Array.create(arrayBuffer, numBytes);
+
+    array.set(testData);
+    assertIsTestData(array);
+  }
+
+  public void testInitWithArrayBufferAndOffsetAndLength() {
+    ArrayBuffer arrayBuffer = ArrayBuffer.create((testData.length + 2) * numBytes);
+
+    Uint32Array array = Uint32Array.create(arrayBuffer, numBytes, testData.length);
+
+    array.set(testData);
+    assertIsTestData(array);
+  }
+
+  public void testInitWithJavaArray() {
+    Uint32Array array = Uint32Array.create(testData);
+
+    assertIsTestData(array);
+  }
+
+  public void testInitWithJsArray() {
+    Uint32Array array = Uint32Array.create(testDataJsArray());
+
+    assertIsTestData(array);
+  }
+
+  public void testInitWithTypedArray() {
+    Uint32Array arraySrc = Uint32Array.create(testData);
+
+    Uint32Array array = Uint32Array.create(arraySrc);
+
+    assertIsTestData(array);
+  }
+
+  public void testSetJavaArray() {
+    Uint32Array array = Uint32Array.create(testData.length);
+
+    array.set(testData);
+    assertIsTestData(array);
+  }
+
+  public void testSetJavaArrayWithOffset() {
+    Uint32Array array = Uint32Array.create(testData.length);
+
+    array.set(testDataPart1);
+    array.set(testDataPart2, 4);
+    assertIsTestData(array);
+  }
+
+  public void testSetJsArray() {
+    Uint32Array array = Uint32Array.create(testData.length);
+
+    array.set(testDataJsArray());
+    assertIsTestData(array);
+  }
+
+  public void testSetJsArrayWithOffset() {
+    Uint32Array array = Uint32Array.create(testData.length);
+
+    array.set(testDataJsArrayPart1());
+    array.set(testDataJsArrayPart2(), 4);
+    assertIsTestData(array);
+  }
+
+  public void testSetTypedArray() {
+    Uint32Array arraySrc = Uint32Array.create(testData);
+
+    Uint32Array array = Uint32Array.create(testData.length);
+
+    array.set(arraySrc);
+    assertIsTestData(array);
+  }
+
+  public void testSetTypedArrayWithOffset() {
+    Uint32Array arraySrc1 = Uint32Array.create(testDataPart1);
+    Uint32Array arraySrc2 = Uint32Array.create(testDataPart2);
+
+    Uint32Array array = Uint32Array.create(testData.length);
+
+    array.set(arraySrc1);
+    array.set(arraySrc2, 4);
+    assertIsTestData(array);
+  }
+
+  public void testSetValues() {
+    Uint32Array array = Uint32Array.create(testData.length);
+
+    for (int i = 0; i < testData.length; i++) {
+      array.set(i, testData[i]);
+    }
+
+    assertIsTestData(array);
+  }
+
+  public void testValueRange() {
+    Uint32Array array = Uint32Array.create(2);
+
+    array.set(0, 0);
+    // 2^32-1
+    array.set(1, 4294967295L);
+
+    assertEquals(0, array.get(0));
+    assertEquals(4294967295L, array.getLong(1));
+  }
+
+  @DoNotRunWith(Platform.Devel)
+  public void testValueRangeProdOnly() {
+    Uint32Array array = Uint32Array.create(testDataValueRange());
+
+    assertEquals(4294967295L, array.getLong(0));
+    assertEquals(0, array.get(1));
+    assertEquals(4294967295L, array.getLong(2));
+    assertEquals(0, array.get(3));
+  }
+
+  private void assertIsTestData(Uint32Array array) {
+    assertEquals(testData.length, array.getLength());
+
+    for (int i = 0; i < testData.length; i++) {
+      assertEquals(testData[i], array.get(i));
+    }
+  }
+}
