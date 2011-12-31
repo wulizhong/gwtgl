86510a42d0b156b0ff54cd4f734dff5825f04a6c
soenke.sothmann@gmail.com <soenke.sothmann@gmail.com@6236e0a2-f3dd-11de-acd2-8fd6687fc178>
2011-12-31 14:38:32 +0000

    Cleanup of TypedArrayTest

--- trunk/GwtGL/src/test/java/com/google/gwt/typedarrays/client/TypedArrayTest.java
+++ trunk/GwtGL/src/test/java/com/google/gwt/typedarrays/client/TypedArrayTest.java
@@ -29,59 +29,6 @@ public class TypedArrayTest extends GWTTestCase {
     return "com.google.gwt.typedarrays.TypedArrays";
   }
 
-  public void testDifferentOffset() {
-    ArrayBuffer arrayBuffer = ArrayBuffer.create(8);
-    Int32Array array1 = Int32Array.create(arrayBuffer);
-    Int32Array array2 = Int32Array.create(arrayBuffer, 4);
-
-    array1.set(0, 1337);
-    array1.set(1, 1338);
-
-    assertEquals(1338, array2.get(0));
-  }
-
-  public void testFloatArrayFromJavaArray() {
-    Float32Array array = Float32Array.create(new float[] {0.1f, 1337.0f});
-
-    assertEquals(0.1f, array.get(0));
-    assertEquals(1337.0f, array.get(1));
-  }
-
-  public void testIntArrayFromJavaArray() {
-    Int32Array array = Int32Array.create(new int[] {0, 1, 2, 3, 4, 5});
-    assertEquals(0, array.get(0));
-    assertEquals(1, array.get(1));
-    assertEquals(2, array.get(2));
-    assertEquals(3, array.get(3));
-    assertEquals(4, array.get(4));
-    assertEquals(5, array.get(5));
-  }
-
-  public void testIntArraySetIntArray() {
-    Int32Array array = Int32Array.create(2);
-    Int32Array array2 = Int32Array.create(new int[] {1337, 1338});
-
-    array.set(array2);
-    assertEquals(1337, array.get(0));
-    assertEquals(1338, array.get(1));
-  }
-
-  public void testIntArraySetJavaArray() {
-    Int32Array array = Int32Array.create(3);
-
-    // set 2 values at offset 0
-    array.set(new int[] {1337, 1338});
-
-    assertEquals(1337, array.get(0));
-    assertEquals(1338, array.get(1));
-
-    // set 2 values at offset 1
-    array.set(new int[] {1, 2}, 1);
-    assertEquals(1337, array.get(0));
-    assertEquals(1, array.get(1));
-    assertEquals(2, array.get(2));
-  }
-
   public void testIntToFloatConversion() {
     ArrayBuffer arrayBuffer = ArrayBuffer.create(4);
     Int32Array int32Array = Int32Array.create(arrayBuffer);
@@ -139,65 +86,6 @@ public class TypedArrayTest extends GWTTestCase {
 
     assertEquals(13, array2.get(0));
     assertEquals(37, array2.get(1));
-  }
-
-  public void testUint16ValueRange() {
-    Uint16Array uint16Array = Uint16Array.create(4);
-
-    // 2^16-1
-    uint16Array.set(0, 65535);
-    // 0 is the min value for unsigned datatypes
-    uint16Array.set(1, 0);
-    // negative value
-    uint16Array.set(2, -1);
-    // 2^16 -> overflow
-    uint16Array.set(3, 65536);
-
-    assertEquals(65535, uint16Array.get(0));
-    assertEquals(0, uint16Array.get(1));
-    assertEquals(65535, uint16Array.get(2));
-    assertEquals(0, uint16Array.get(3));
-  }
-
-  public void testUint8ClampedValueRange() {
-    if (!Uint8ClampedArray.isSupported()) {
-      // nothing to test
-      return;
-    }
-    Uint8ClampedArray uint8Array = Uint8ClampedArray.create(4);
-
-    // 2^8-1
-    uint8Array.set(0, 255);
-    // 0 is the min value for unsigned datatypes
-    uint8Array.set(1, 0);
-    // negative value
-    uint8Array.set(2, -1);
-    // 2^8
-    uint8Array.set(3, 256);
-
-    assertEquals(255, uint8Array.get(0));
-    assertEquals(0, uint8Array.get(1));
-    // no overflow
-    assertEquals(0, uint8Array.get(2));
-    assertEquals(255, uint8Array.get(3));
-  }
-
-  public void testUint8ValueRange() {
-    Uint8Array uint8Array = Uint8Array.create(4);
-
-    // 2^8-1
-    uint8Array.set(0, 255);
-    // 0 is the min value for unsigned datatypes
-    uint8Array.set(1, 0);
-    // negative value
-    uint8Array.set(2, -1);
-    // 2^8 -> overflow
-    uint8Array.set(3, 256);
-
-    assertEquals(255, uint8Array.get(0));
-    assertEquals(0, uint8Array.get(1));
-    assertEquals(255, uint8Array.get(2));
-    assertEquals(0, uint8Array.get(3));
   }
 
 }
